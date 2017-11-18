c0b3d6bce21be9973da886cc87adcb4cdff6c9cc
Mo Amin <m.alamin92@gmail.com>
2017-11-18 16:19:45 -0500

    remove bootstrap cdn and import from npm instead

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -17,10 +17,10 @@ export default class Flipcard extends Component {
 	render() {
 		return (
 			<ReactCardFlip isFlipped={this.state.isFlipped}>
-				<div key="front" style={{width: "100%"}} onClick={this.handleClick.bind(this)}>
+				<div key="front" class="d-flex justify-content-center" style={{width: "100%"}} onClick={this.handleClick.bind(this)}>
 					<img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg" alt="Cute pup."/>
 				</div>
-				<div key="back" style={{width: "100%"}} onClick={this.handleClick.bind(this)}>
+				<div key="back" class="d-flex justify-content-center" style={{width: "100%"}} onClick={this.handleClick.bind(this)}>
 					<img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg" alt="Cute pup."/>
 				</div>
 			</ReactCardFlip>
