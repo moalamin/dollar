91b5f04c51beb2dbe6b82ddf96e868a1285b7c58
Mo Alamin <m.alamin92@gmail.com>
2017-11-17 23:03:11 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+import HomePage from './components/HomePage';
+
+class App extends Component {
+	render () {
+		return (
+			<HomePage/>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (props) => {
+	return (
+		<div className="container-fluid">
+			<div className="jumbotron jumbotron-fluid">
+				<div className="container">
+					<h1 className="d-flex justify-content-center" >Give me a damn dollar!</h1>
+				</div>
+			</div>
+		</div>
+	);
+};
