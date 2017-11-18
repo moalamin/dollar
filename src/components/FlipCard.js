cbaadb23160a1d4f1ad7f4f066091d3cc9579a74
Mo Amin <m.alamin92@gmail.com>
2017-11-18 16:31:50 -0500

    syntax

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -16,12 +16,12 @@ export default class Flipcard extends Component {
 
 	render() {
 		return (
-			<ReactCardFlip isFlipped={this.state.isFlipped}>
-				<div key="front" class="d-flex justify-content-center" style={{width: "100%"}} onClick={this.handleClick.bind(this)}>
-					<img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg" alt="Cute pup."/>
+			<ReactCardFlip flipSpeedBackToFront='1.5' flipSpeedFrontToBack='1.5' isFlipped={this.state.isFlipped}>
+				<div key='front' class='d-flex justify-content-center' style={{width: '100%'}} onClick={this.handleClick.bind(this)}>
+					<img className='card-img-top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg' alt='Cute pup.'/>
 				</div>
-				<div key="back" class="d-flex justify-content-center" style={{width: "100%"}} onClick={this.handleClick.bind(this)}>
-					<img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg" alt="Cute pup."/>
+				<div key='back' class='d-flex justify-content-center' style={{width: '100%'}} onClick={this.handleClick.bind(this)}>
+					<img className='card-img-top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg' alt='Cute pup.'/>
 				</div>
 			</ReactCardFlip>
 		);
