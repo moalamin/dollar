f48fc2bc4948bb6407d588969c25e6325ba41b04
Mo Alamin <m.alamin92@gmail.com>
2017-11-18 15:48:44 -0500

    flipping dollart

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -17,21 +17,11 @@ export default class Flipcard extends Component {
 	render() {
 		return (
 			<ReactCardFlip isFlipped={this.state.isFlipped}>
-				<div key="front" className="card" style={{width: "100%"}}>
+				<div key="front" style={{width: "100%"}} onClick={this.handleClick.bind(this)}>
 					<img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg" alt="Cute pup."/>
-					<div className="card-body">
-						<h4 className="card-title">Card title</h4>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" onClick={this.handleClick.bind(this)} className="btn btn-primary">Go somewhere</a>
-					</div>
 				</div>
-				<div key="back" className="card" style={{width: "100%"}}>
+				<div key="back" style={{width: "100%"}} onClick={this.handleClick.bind(this)}>
 					<img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg" alt="Cute pup."/>
-					<div className="card-body">
-						<h4 className="card-title">Card title</h4>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" onClick={this.handleClick.bind(this)} className="btn btn-danger">Go somewhere</a>
-					</div>
 				</div>
 			</ReactCardFlip>
 		);
