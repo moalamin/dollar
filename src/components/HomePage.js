b458253256140bb0b7f860bee23d3b48ebbe399c
Mo Alamin <m.alamin92@gmail.com>
2017-11-20 00:07:49 -0500

    google fonts

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,12 +4,16 @@ import FlipCard from './FlipCard';
 export default (props) => {
 	return (
 		<div className="container-fluid">
-			<div className="jumbotron jumbotron-fluid">
+			<div className="row">
 				<div className="container">
-					<h1 className="d-flex justify-content-center" >Just give a dollar:</h1>
+					<h1 className="d-flex justify-content-center" >Give a dollar</h1>
 				</div>
 			</div>
-			<FlipCard/>
+			<div className="row">
+				<div className="container">
+					<FlipCard/>
+				</div>
+			</div>
 		</div>
 	);
 };
