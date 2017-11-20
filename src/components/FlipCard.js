31663c018ee75e6e574e2852fe2d2ee24ac99c88
Mo Amin <m.alamin92@gmail.com>
2017-11-20 04:07:51 -0500

    locla version of dollar

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactCardFlip from 'react-card-flip';
+import dollarJPG from '../dollar.jpg';
 
 export default class Flipcard extends Component {
 	constructor(props) {
@@ -18,10 +19,10 @@ export default class Flipcard extends Component {
 		return (
 			<ReactCardFlip flipSpeedBackToFront={1.5} flipSpeedFrontToBack={1.5} isFlipped={this.state.isFlipped}>
 				<div key='front' className='d-flex justify-content-center' style={{width: '100%'}} onClick={this.handleClick.bind(this)}>
-					<img className='img-fluid' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg' alt='Cute pup.'/>
+					<img className='img-fluid' src={dollarJPG} alt='Cute pup.'/>
 				</div>
 				<div key='back' className='d-flex justify-content-center' style={{width: '100%'}} onClick={this.handleClick.bind(this)}>
-					<img className='img-fluid' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg' alt='Cute pup.'/>
+					<img className='img-fluid' src={dollarJPG} alt='Cute pup.'/>
 				</div>
 			</ReactCardFlip>
 		);
