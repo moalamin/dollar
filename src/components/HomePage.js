5475db1da30b358bb53dec3343a41cb612548aa0
Mo Alamin <m.alamin92@gmail.com>
2017-11-20 00:30:33 -0500

    rearange

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,13 +4,13 @@ import FlipCard from './FlipCard';
 export default (props) => {
 	return (
 		<div className="container-fluid">
-			<div className="row">
-				<div className="container">
-					<h1 className="d-flex justify-content-center" >Give a dollar</h1>
+			<div className="container d-flex justify-content-center">
+				<div className="row">
+					<h1>Give a Dollar</h1>
 				</div>
 			</div>
 			<div className="row">
-				<div className="container">
+				<div className="container-fluid dollar-img">
 					<FlipCard/>
 				</div>
 			</div>
