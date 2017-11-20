303cd3db3e8240a2510cbeb4d3e0e7431f92be7b
Mo Alamin <m.alamin92@gmail.com>
2017-11-19 22:31:38 -0500

    prop fixes

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -16,11 +16,11 @@ export default class Flipcard extends Component {
 
 	render() {
 		return (
-			<ReactCardFlip flipSpeedBackToFront='1.5' flipSpeedFrontToBack='1.5' isFlipped={this.state.isFlipped}>
-				<div key='front' class='d-flex justify-content-center' style={{width: '100%'}} onClick={this.handleClick.bind(this)}>
+			<ReactCardFlip flipSpeedBackToFront={1.5} flipSpeedFrontToBack={1.5} isFlipped={this.state.isFlipped}>
+				<div key='front' className='d-flex justify-content-center' style={{width: '100%'}} onClick={this.handleClick.bind(this)}>
 					<img className='card-img-top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg' alt='Cute pup.'/>
 				</div>
-				<div key='back' class='d-flex justify-content-center' style={{width: '100%'}} onClick={this.handleClick.bind(this)}>
+				<div key='back' className='d-flex justify-content-center' style={{width: '100%'}} onClick={this.handleClick.bind(this)}>
 					<img className='card-img-top' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Onedolar2009series.jpg/1200px-Onedolar2009series.jpg' alt='Cute pup.'/>
 				</div>
 			</ReactCardFlip>
