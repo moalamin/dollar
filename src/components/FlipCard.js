3e9b45516b48b5c8b62c287c68bf7ac8c34bcaff
Mo Alamin <m.alamin92@gmail.com>
2017-11-20 18:32:27 -0500

    embed form

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -15,14 +15,82 @@ export default class Flipcard extends Component {
 		this.setState({ isFlipped: !this.state.isFlipped });
 	}
 
+	handleSubmit(e) {
+		e.stopPropagation()
+	}
+
 	render() {
 		return (
 			<ReactCardFlip flipSpeedBackToFront={1.5} flipSpeedFrontToBack={1.5} isFlipped={this.state.isFlipped}>
-				<div key='front' className='d-flex justify-content-center' style={{width: '100%'}} onClick={this.handleClick.bind(this)}>
-					<img className='img-fluid' src={dollarJPG} alt='Dollar Bill'/>
+				<div
+					key="front"
+					className="d-flex justify-content-center"
+					style={{ width: '100%' }}
+					onClick={this.handleClick.bind(this)}>
+					<img className="img-fluid" src={dollarJPG} alt="Dollar Bill" />
 				</div>
-				<div key='back' className='d-flex justify-content-center' style={{width: '100%'}} onClick={this.handleClick.bind(this)}>
-					<div className="dollar-form" style={{width: '1200px', height: '512px', display: 'block'}}></div>
+				<div
+					key="back"
+					className="d-flex justify-content-center"
+					style={{ width: '100%' }}
+					onClick={this.handleClick.bind(this)}>
+					<div
+						className="dollar-form"
+						style={{
+							width: '1200px',
+							height: '512px',
+							display: 'block'
+						}}>
+						<form>
+							<div className="container">
+								<div className="row">
+									<div className="col-md-6">
+										<div className="form-group">
+											<label htmlFor="inputEmail4">Email</label>
+											<input
+												type="email"
+												className="form-control"
+												id="inputEmail4"
+												placeholder="Email"
+											/>
+										</div>
+										<div className="form-group">
+											<label htmlFor="inputPassword4">Password</label>
+											<input
+												type="password"
+												className="form-control"
+												id="inputPassword4"
+												placeholder="Password"
+											/>
+										</div>
+									</div>
+									<div className="col-md-6">
+										<div className="form-group">
+											<label htmlFor="inputEmail4">Email</label>
+											<input
+												type="email"
+												className="form-control"
+												id="inputEmail4"
+												placeholder="Email"
+											/>
+										</div>
+										<div className="form-group">
+											<label htmlFor="inputPassword4">Password</label>
+											<input
+												type="password"
+												className="form-control"
+												id="inputPassword4"
+												placeholder="Password"
+											/>
+										</div>
+									</div>
+									<button onClick={this.handleSubmit.bind(this)} type="submit" className="btn btn-primary">
+										Sign in
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
 				</div>
 			</ReactCardFlip>
 		);
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import FlipCard from './FlipCard';
 
-export default (props) => {
+export default props => {
 	return (
 		<div className="container">
 			<div className="row d-flex justify-content-center">
-				<h1 className="display-1" style={{textAlign: 'center'}}>Give a Dollar</h1>
+				<h1 className="display-1" style={{ textAlign: 'center' }}>
+					Give a Dollar
+				</h1>
 			</div>
 			<div className="row d-none d-sm-block">
 				<div className="col-md-12">
-					<div className="dollar-bill" style={{width: '100%'}}>
-						<FlipCard/>
+					<div className="dollar-bill" style={{ width: '100%' }}>
+						<FlipCard />
 					</div>
 				</div>
 			</div>
 			<div className="row d-block d-sm-none">
 				<div className="col-md-12">
-					<div className="dollar-bill" style={{width: '100%'}}>
+					<div className="dollar-bill" style={{ width: '100%' }}>
 						<h1>Show only on small</h1>
 					</div>
 				</div>
